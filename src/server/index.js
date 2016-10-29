b19c9d2173902f9f103af704f4aa1c9ac99eb9e0
David Delo <ddelo@me.com>
2016-10-29 16:13:51 -0700

    Modify build config

--- src/server/index.js
+++ src/server/index.js
@@ -1,3 +1,2 @@
 require('babel-register');
-process.env = JSON.parse(JSON.stringify(process.env));
 require('./app');
--- webpack.config.js
+++ webpack.config.js
@@ -11,45 +11,70 @@ var vendor = [
   'react-router'
 ];
 
-//    'webpack/hot/dev-server',
-//    'webpack-dev-server/client?http://localhost:3000',
+var commonLoaders = [
+  {
+    test: /\.jsx?$/,
+    loaders: ['babel'],
+    exclude: /node_modules/
+  },
+	{
+    test: /\.png$/,
+    loader: 'url-loader'
+  },
+	{
+    test: /\.jpg$/,
+    loader: 'file-loader'
+  },
+];
 
-var config = {
-  entry: {
-    server: './src/server/index.js',
-    client: './src/client/index.js',
-    vendor
-  },
-  output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: '[name]-[chunkhash:8].js',
-    chunkFilename: '[name]-[chunkhash:8].js'
-  },
-  module: {
-    preLoaders: [
-      {
-        test: /\.jsx?$/,
-        loaders: ['eslint-loader'],
-        exclude: /node_modules/
-      }
-    ],
-    loaders: [
-      {
-        test: /\.css?$/,
-        loader: `style!css-loader?modules&importLoaders=1&${moduleName}`,
-        include: __dirname
-      },
-      {
-        test: /\.scss$/,
-        loader: `style!css-loader?modules&importLoaders=1&${moduleName}!sass-loader`
-      },
-      {
-        test: /\.jsx?$/,
-        loaders: ['babel'],
-        exclude: /node_modules/
-      }
-    ]
+var preLoaders = [
+  {
+    test: /\.jsx?$/,
+    loaders: ['eslint-loader'],
+    exclude: /node_modules/
   }
-}
+];
 
-module.exports = config;
+module.exports = [
+  {
+		name: 'client',
+    entry: {
+      client: './src/client/index.js',
+      vendor
+    },
+    output: {
+      path: path.resolve(__dirname, 'dist'),
+      filename: '[name]-[chunkhash:8].js',
+      chunkFilename: '[name]-[chunkhash:8].js'
+    },
+    module: {
+      preLoaders: preLoaders,
+			loaders: commonLoaders.concat([
+        {
+          test: /\.css?$/,
+          loader: `style!css-loader?modules&importLoaders=1&${moduleName}`,
+          include: __dirname
+        },
+        {
+          test: /\.scss$/,
+          loader: `style!css-loader?modules&importLoaders=1&${moduleName}!sass-loader`
+        }
+      ])
+		}
+  },
+  {
+    name: 'server',
+    entry: './src/server/index.js',
+    target: 'node',
+    output: {
+      path: path.resolve(__dirname, 'dist'),
+      filename: 'app.js',
+      libraryTarget: 'commonjs2'
+    },
+    externals: /^[a-z\-0-9]+$/,
+    module: {
+      preLoaders: preLoaders,
+			loaders: commonLoaders
+		}
+  }
+];
