2b710a9d084f0cdf3a751dbd676aff785e9c7ace
David Delo <dave@daviddelo.com>
2017-12-31 14:29:46 -0800

    Updated files for new website

---
+++ webpack.config.js
@@ -0,0 +1,58 @@
+import path from 'path';
+import webpack from 'webpack';
+
+const webpackConfig = {
+  entry: {
+    client: './src/client/index.js',
+    server: './src/server/app.js'
+  },
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: '[name]-[chunkhash:8].js',
+    publicPath: '/public/',
+    chunkFilename: '[name]-[id]-[chunkhash:8].js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader'
+      }
+    ]
+  },
+  performance: {
+    hints: 'warning', // enum
+    maxAssetSize: 200000, // int (in bytes),
+    maxEntrypointSize: 400000
+  },
+  plugins: [
+    new webpack.optimize.CommonsChunkPlugin({
+      name: 'vendor',
+      minChunks: module => module.context && module.context.includes('node_modules')
+    }),
+    new webpack.optimize.CommonsChunkPlugin({
+      name: 'manifest',
+      minChunks: Infinity
+    }),
+    new webpack.optimize.UglifyJsPlugin({
+      output: {
+        comments: false
+      }
+    }),
+    new webpack.HashedModuleIdsPlugin({
+      hashFunction: 'sha256',
+      hashDigest: 'hex',
+      hashDigestLength: 20
+    })
+  ],
+  stats: {
+    assets: true,
+    colors: true,
+    errors: true,
+    errorDetails: true,
+    hash: true
+  }
+};
+
+export default webpackConfig;
